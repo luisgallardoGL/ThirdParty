2bd25c06b3869e480ff95bf1ef4757e95dec8732
stuartleija <stuart.leija@davita.com>
2017-06-22 11:50:56 -0600

    removed rounding

--- BatchFiles/FetchAndBuild.bat
+++ BatchFiles/FetchAndBuild.bat
@@ -53,15 +53,6 @@ if errorlevel 1 (
    exit /b %errorlevel%
 )
 popd
-pushd Rounding_V2
-ECHO Pulling Rounding_V2
-git rev-parse --abbrev-ref HEAD
-git pull
-if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel% in Rounding_V2
-   exit /b %errorlevel%
-)
-popd
 pause
 pushd DataProviders
 ECHO Building DataProviders...
