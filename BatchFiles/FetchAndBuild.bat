013b3a506e5034af92c9ca89fb11ca57fda970ff
James Mruzik <JMRUZIK@davita.corp>
2017-07-12 17:46:15 -0600

    Updated FetchAndBuild.bat - JM

--- BatchFiles/FetchAndBuild.bat
+++ BatchFiles/FetchAndBuild.bat
@@ -1,99 +1,143 @@
 @ECHO OFF
+set ORIGINAL_DIR=%CD%
+pushd ..\..
 if not defined DevEnvDir (
-	ECHO Need to define DevEnvDir
 	if exist "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" goto :SET2017
 	if exist "%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat" goto :SET2015
 	if exist "%PROGRAMFILES(X86)%\Microsoft Visual Studio 12.0\Common7\Tools\\VsDevCmd.bat" goto :SET2013
 
 	:SET2017
 	CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-	echo Set for 2017
-	goto :RunFetchAndBuild
+	goto :FetchQuestion
 
 	:SET2015
 	CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-	echo Set for 2015
-	goto :RunFetchAndBuild
+	goto :FetchQuestion
 
 	:SET2013
 	CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
-	echo set for 2013
-	goto :RunFetchAndBuild
+	goto :FetchQuestion
 )
 
-:RunFetchAndBuild
-echo Ready...
-
-pushd ..\..
-
+:FetchQuestion
+echo CURRENT BRANCHES
 pushd DataProviders
-ECHO Pulling DataProviders
-git rev-parse --abbrev-ref HEAD
-git pull
-if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel% in DataProviders
-   exit /b %errorlevel%
-)
+for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set branch=%%i
+echo DataProviders:  %branch%
 popd
 pushd Common
-ECHO Pulling Common
-git rev-parse --abbrev-ref HEAD
-git pull
-if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel% in Common
-   exit /b %errorlevel%
-)
+for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set branch=%%i
+echo Common:         %branch%
+popd
+pushd Capella-API_V2
+for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set branch=%%i
+echo Capella-API_V2: %branch%
 popd
 pushd Capella
-ECHO Pulling Capella
-git rev-parse --abbrev-ref HEAD
-git pull
-if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel% in Capella
-   exit /b %errorlevel%
-)
+for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set branch=%%i
+echo Capella:        %branch%
 popd
+
+CHOICE /M "Do you want to Pull from all repos?"
+if %errorlevel% == 1 goto :Fetch
+if %errorlevel% == 2 goto :Build
+
+:Fetch
+pushd DataProviders
+ECHO Pulling DataProviders
+git pull
+if not %errorlevel% == 0 ( goto :Error )
+popd
+
+pushd Common
+ECHO Pulling Common
+git pull
+if not %errorlevel% == 0 ( goto :Error )
+popd
+
+pushd Capella-API_V2
+ECHO Pulling Capella-API_V2
+git pull
+if not %errorlevel% == 0 ( goto :Error )
+popd
+
+pushd Capella
+ECHO Pulling Capella
+git pull
+if not %errorlevel% == 0 ( goto :Error )
+popd
+
+:Build
 pushd DataProviders
 ECHO Building DataProviders...
+nuget restore DataProviders.sln -Verbosity quiet
 msbuild DataProviders.sln /m /t:rebuild /verbosity:quiet /p:WarningLevel=0 /clp:ErrorsOnly /nologo
-if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel% in DataProviders
-   exit /b %errorlevel%
+if not %errorlevel% == 0 (
+   goto :Error
 ) else (
 	ECHO Done.
 )
 popd
 pushd Common
 ECHO Building Common...
+nuget restore Common.sln -Verbosity quiet
 msbuild Common.sln /m /t:rebuild /verbosity:quiet /p:WarningLevel=0 /clp:ErrorsOnly /nologo
-if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel% in Common
-   exit /b %errorlevel%v
+if not %errorlevel% == 0 (
+   goto :Error
 ) else (
 	ECHO Done.
 )
 popd
+
+pushd Capella-API_V2
+ECHO Building Capella-API_V2...
+nuget restore CapellaMobileServices.sln -Verbosity quiet
+msbuild CapellaMobileServices.sln /m /t:rebuild /verbosity:quiet /p:WarningLevel=0 /clp:ErrorsOnly /nologo
+if not %errorlevel% == 0 (
+	goto :Error
+) else (
+	ECHO Done.
+)
+popd
+
 pushd Capella
 pushd Domain
 ECHO Building Domain...
+nuget restore Domain.sln -Verbosity quiet
 msbuild Domain.sln /m /t:rebuild /verbosity:quiet /p:WarningLevel=0 /clp:ErrorsOnly /nologo
-if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel% in Domain
-   exit /b %errorlevel%
+if not %errorlevel% == 0 (
+   goto :Error
 ) else (
 	ECHO Done.
 )
 popd
 pushd Services
 ECHO Building Services...
+nuget restore Services.sln -Verbosity quiet
 msbuild Services.sln /m /t:rebuild /verbosity:quiet /p:WarningLevel=0 /clp:ErrorsOnly /nologo
-if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel% in Services
-   exit /b %errorlevel%
+if not %errorlevel% == 0 (
+   goto :Error
 ) else (
 	ECHO Done.
 )
 popd
-popd
-popd
+
+pushd UI
+ECHO Building FireflyUI...
+nuget restore UI.sln -Verbosity quiet
+msbuild UI.sln /m /t:rebuild /verbosity:quiet /p:WarningLevel=0 /clp:ErrorsOnly /nologo
+if not %errorlevel% == 0 (
+   goto :Error
+) else (
+	ECHO Done.
+	goto :Exit
+)
+
+:Error
+echo Error level given is %errorlevel%
+chdir /d %ORIGINAL_DIR% 
+exit /b %errorlevel%
+
+:Exit
+chdir /d %ORIGINAL_DIR% 
 echo Completed.
